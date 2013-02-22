e82e8cb3b4e855bf53d7f7eb3fe8dd079e8abb60
Bastian Krol <bastian.krol@web.de>
2013-02-23 00:15:25 +0100

    more tests for adding and displaying details

--- features/add-audiobook.feature
+++ features/add-audiobook.feature
@@ -6,9 +6,15 @@ Feature: Add an audiobook to the collection
   Scenario: Add a new audiobook to the collection via menu
     When I visit the list of audiobooks
     And select "Add new Audiobook" from the menu
-    Then I see the form...
+    Then I see the form for adding a new audiobook
+    When I hack in a new audiobook
+    And save it
+    Then I see the new audiobook in the list of audiobooks
 
   Scenario: Add a new audiobook to the collection via button on list page
     When I visit the list of audiobooks
     And click the button to add a new audiobook
-    Then I see the form...
+    Then I see the form for adding a new audiobook
+    When I hack in a new audiobook
+    And save it
+    Then I see the new audiobook in the list of audiobooks
--- features/step_definitions/audiobooks.rb
+++ features/step_definitions/audiobooks.rb
@@ -28,8 +28,25 @@ When /^select "(.*?)" from the menu$/ do |option|
   click_link option
 end
 
+When /^click on "(.*?)"$/ do |link|
+  click_link link
+end
+
 When /^click the button to add a new audiobook$/ do
   click_link 'Add Audiobook'
+end
+
+When /^I hack in a new audiobook$/ do
+  fill_in('title', :with => 'Cucumber for Dummies')
+  fill_in('author_first_name', :with => 'Au')
+  fill_in('author_last_name', :with => 'Thor')
+  fill_in('voice_artist_first_name', :with => 'Voice')
+  fill_in('voice_artist_last_name', :with => 'Artist')
+  fill_in('description', :with => 'This is a lengthy, boring description about the audiobook.')
+end
+
+When /^save it$/ do
+  click_button 'create'
 end
 
 Then /^I see the application name$/ do
@@ -61,6 +78,24 @@ Then /^(?:I see )?"(.*?)" listed (first|second|third)$/ do |title, position|
   end
 end
 
-Then /^I see the form\.\.\.$/ do
+Then /^I see the form for adding a new audiobook$/ do
   page.find('h1').should have_content 'Add a New Audiobook'
+end
+
+Then /^I see the new audiobook in the list of audiobooks$/ do
+  # somehow, when running on PhantomJS the backend POST call to create the new
+  # audiobook takes effect only *after* the frontend redirect to /audiobooks
+  # has taken place. When using a real browser, this problem does not occur
+  # (probably because PhantomJS is faster than a real browser).
+  # Note: wait_until or even sleep won't help here because AngularJS has already
+  # executed the request to /audiobooks. It won't fetch the collection from the
+  # backend again by itself.
+  # Ugly hack: After creating the audiobook, we just do a full page reload. :-(
+  visit ui_url '/index.html'
+  page.should have_content('Cucumber for Dummies')
+  page.should have_content('Thor, Au')
+end
+
+Then /^I see the details page for "(.*?)"$/ do |audiobook_title|
+  page.find('h1').should(have_content('Coraline'))
 end
