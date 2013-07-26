2b2b8f2b0a88dd407c3327cc55bb92325474606f
Bastian Krol <basti1302>
2013-07-26 21:47:21 +0200

    typo

--- features/step_definitions/audiobooks.rb
+++ features/step_definitions/audiobooks.rb
@@ -8,7 +8,7 @@ When /^I visit the list of audiobooks$/ do
   visit ui_url '/index.html'
 end
 
-Then /^I see all audiobooks?$/ do
+Then /^I see all audiobooks$/ do
   page.should have_content 'Coraline'
   page.should have_content 'Man In The Dark'
   page.should have_content 'Siddhartha'
